1eec6a7d7e3970caca2426cf3144263273d2e48b
Emaddbutt <163984067+Emaddbutt@users.noreply.github.com>
2024-03-23 17:02:44 +0500

    Update axios.js

--- React login project/src/api/axios.js
+++ React login project/src/api/axios.js
@@ -1,12 +1,24 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-    baseURL: ''
-});
-
+const axiosInstance = axios.create();
+const API_URL = '';
 axiosInstance.interceptors.request.use(config => {
     config.withCredentials = true; // Set withCredentials for all requests
     return config;
 });
 
-export default axiosInstance;
+const sendRequest = async (urlObject, payload, type) => {
+    try {
+        const response = await axiosInstance({
+            method: urlObject.method,
+            url: `${API_URL}/${urlObject.endpoint}/${type}`,
+            data: payload
+        });
+        return response.data;
+    } catch (error) {
+        // Handle errors here
+        throw error;
+    }
+};
+
+export default sendRequest;
