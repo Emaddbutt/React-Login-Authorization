60eb912d16794f14b3dc890e53657276dbcf5155
Emaddbutt <163984067+Emaddbutt@users.noreply.github.com>
2024-03-23 17:55:37 +0500

    Update axios.js

--- React login project/src/api/axios.js
+++ React login project/src/api/axios.js
@@ -1,24 +1,12 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create();
-const API_URL = '';
+const axiosInstance = axios.create({
+    baseURL: 'http://https://drab-red-codfish-cape.cyclic.app/'
+});
+
 axiosInstance.interceptors.request.use(config => {
     config.withCredentials = true; // Set withCredentials for all requests
     return config;
 });
 
-const sendRequest = async (urlObject, payload, type) => {
-    try {
-        const response = await axiosInstance({
-            method: urlObject.method,
-            url: `${API_URL}/${urlObject.endpoint}/${type}`,
-            data: payload
-        });
-        return response.data;
-    } catch (error) {
-        // Handle errors here
-        throw error;
-    }
-};
-
-export default sendRequest;
+export default axiosInstance;
